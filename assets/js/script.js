fb7bab5c6034d48023144ac8ac2c9339697423b6
Sash <sasheng04@gmail.com>
2022-05-06 16:17:55 +0000

    Adjust basic structure to index.html and style.css

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,9 @@ window.addEventListener('load', init);
 
 // Available Levels
 const levels = {
-    easy: 15,
-    medium: 10,
-    hard: 5
+    easy: 5,
+    medium: 3,
+    hard: 2
 }
 
 // To change level
@@ -94,18 +94,18 @@ const words = [
 const settingOption = document.getElementById('optionBtn');
 const menuSlideElt = document.getElementById('menuSlide');
 
-settingOption.addEventListener('click', function(){
+settingOption.addEventListener('click', function () {
     menuSlideElt.classList.toggle("slideIn");
 });
 
 
 // Seclect level
-function setlevel(e){
-    if(e.target === easyBtn){
+function setlevel(e) {
+    if (e.target === easyBtn) {
         currentLevel = levels.easy;
-    }else if(e.target === mediumBtn){
+    } else if (e.target === mediumBtn) {
         currentLevel = levels.medium;
-    }else if(e.target === hardBtn){
+    } else if (e.target === hardBtn) {
         currentLevel = levels.hard;
     }
     console.log(currentLevel);
@@ -113,7 +113,7 @@ function setlevel(e){
 }
 
 // Initialize Game
-function init(){
+function init() {
     // Show number of sec in UI
     seconds.innerHTML = currentLevel;
     // Load word from array
@@ -129,9 +129,9 @@ function init(){
 }
 
 //Start match
-function startMatch(){
+function startMatch() {
     wordInput.value = wordInput.value.toLowerCase();
-    if(matchWords()){
+    if (matchWords()) {
         isPlaying = true;
         time = currentLevel + 1;
         showWord(words);
@@ -140,14 +140,14 @@ function startMatch(){
     }
 
     // If score is -1 display zero
-    if(score === -1){
+    if (score === -1) {
         scoreDisplay.innerHTML = 0;
-    }else{
+    } else {
         scoreDisplay.innerHTML = score;
         highScoreElt.innerHTML = score;
-        
-        if(score >= maxScore){
-            localStorage.setItem('highScore',score);
+
+        if (score >= maxScore) {
+            localStorage.setItem('highScore', score);
         }
     }
     maxScore = localStorage.getItem('highScore');
@@ -156,19 +156,19 @@ function startMatch(){
 }
 
 // Match currentWord to wordInput
-function matchWords(){
-    
-        if(wordInput.value === currentWord.innerHTML){
-            message.innerHTML = 'Great';
-            return true;
-        }else{
-            message.innerHTML = '...';
-            return false;
-        }
+function matchWords() {
+
+    if (wordInput.value === currentWord.innerHTML) {
+        message.innerHTML = 'Great';
+        return true;
+    } else {
+        message.innerHTML = '...';
+        return false;
+    }
 }
 
 // Pick and show random word
-function showWord(word){
+function showWord(word) {
     // Generate random array index
     const randIndex = Math.floor(Math.random() * words.length);
     // Output random word
@@ -176,12 +176,12 @@ function showWord(word){
 }
 // Countdown timer
 
-function countdown(){
+function countdown() {
     // Make sure time is not runout
-    if(time > 0){
+    if (time > 0) {
         // decrement
         time--;
-    }else if(time === 0){
+    } else if (time === 0) {
         // Game is over
         isPlaying = false;
     }
@@ -190,8 +190,8 @@ function countdown(){
 }
 
 // Check game status
-function checkStatus(){
-    if(!isPlaying && time === 0){
+function checkStatus() {
+    if (!isPlaying && time === 0) {
         message.innerHTML = 'Game Over!';
         score = -1;
     }
